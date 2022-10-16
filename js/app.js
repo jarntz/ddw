f188ed25ee21ed07bc309038154b2ff18507e841
Jordy Arntz <hello@jordyarntz.com>
2022-10-16 12:21:30 +0200

    Colors

--- js/app.js
+++ js/app.js
@@ -17,7 +17,6 @@ function setup() {
 
 function draw() {
   data.update();
-  background(0);
   // image(data.output.video, 0, 0); // Face cam
 
   if (data.output.expressions) {
@@ -31,47 +30,53 @@ function draw() {
       text(Object.keys(data.output.expressions)[count - 1], 50, windowH - 50 * count); // write object key on the screen
 
       let emotion = Object.keys(data.output.expressions)[0];
-      let gradientStep = null;
 
       switch (emotion) {
         case 'neutral':
-          gradientStep1 = color(255, 0, 0);
-          gradientStep2 = color(0, 255, 0);
+          background(218, 49, 218);
+          gradientStep1 = color(111, 23, 110);
+          gradientStep2 = color(221, 45, 219);
           break;
         case 'happy':
-          gradientStep1 = color(0, 0, 255);
-          gradientStep2 = color(0, 255, 0);
+          background(185, 125, 32);
+          gradientStep1 = color(124, 85, 30);
+          gradientStep2 = color(248, 170, 59);
           break;
         case 'angry':
-          gradientStep1 = color(255, 0, 0);
-          gradientStep2 = color(0, 255, 0);
+          background(220, 95, 80);
+          gradientStep1 = color(107, 36, 34);
+          gradientStep2 = color(214, 71, 67);
           break;
         case 'disgusted':
-          gradientStep1 = color(255, 0, 0);
-          gradientStep2 = color(0, 255, 0);
+          background(220, 95, 80);
+          gradientStep1 = color(1, 61, 31);
+          gradientStep2 = color(1, 122, 62);
           break;
         case 'surprised':
-          gradientStep1 = color(255, 0, 0);
-          gradientStep2 = color(0, 255, 0);
+          background(185, 125, 32);
+          gradientStep1 = color(124, 85, 30);
+          gradientStep2 = color(248, 170, 59);
           break;
         case 'sad':
-          gradientStep1 = color(255, 0, 0);
-          gradientStep2 = color(0, 255, 0);
+          background(111, 148, 185);
+          gradientStep1 = color(127, 170, 214);
+          gradientStep2 = color(255, 255, 255);
           break;
         case 'fearful':
-          gradientStep1 = color(255, 0, 0);
-          gradientStep2 = color(0, 255, 0);
+          background(218, 49, 218);
+          gradientStep1 = color(111, 23, 110);
+          gradientStep2 = color(221, 45, 219);
           break;
         default:
       }
 
       let gradX = 0;
       let gradY = 0;
-      let gradient = ctx.createRadialGradient(0, 0, 50, gradX, gradY, 0);
+      let gradient = ctx.createRadialGradient(0, 0, 200, gradX, gradY, 0);
       gradient.addColorStop(0, gradientStep1);
       gradient.addColorStop(1, gradientStep2);
       ctx.fillStyle = gradient;
-      ellipse(0, 0, 100, 100);
+      ellipse(0, 0, 200, 200);
       translate(width / 2, height / 2);
     }
   }
